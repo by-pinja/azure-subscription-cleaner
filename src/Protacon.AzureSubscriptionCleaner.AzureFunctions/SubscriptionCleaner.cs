4b4cad0262e219ba2f2ce849aebbd791114d02da
Hekku2 <hekku2@gmail.com>
2019-08-30 10:19:41 +0300

    Add analyzator and fix analyzator issues

--- src/Protacon.AzureSubscriptionCleaner.AzureFunctions/SubscriptionCleaner.cs
+++ src/Protacon.AzureSubscriptionCleaner.AzureFunctions/SubscriptionCleaner.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
 namespace Protacon.AzureSubscriptionCleaner.AzureFunctions
@@ -23,19 +21,34 @@ namespace Protacon.AzureSubscriptionCleaner.AzureFunctions
         [FunctionName(nameof(TimerStart))]
         public async Task TimerStart([TimerTrigger("0 2 1 * * *")] TimerInfo timer, [OrchestrationClient] IDurableOrchestrationClient starter)
         {
+            if (timer is null)
+            {
+                throw new System.ArgumentNullException(nameof(timer));
+            }
+
+            if (starter is null)
+            {
+                throw new System.ArgumentNullException(nameof(starter));
+            }
+
             _logger.LogTrace("{class}, Next: {next}, Last: {last}", nameof(TimerStart), timer.ScheduleStatus.Next, timer.ScheduleStatus.Last);
-            string instanceId = await starter.StartNewAsync(nameof(OchestrateSubscriptionCleanUp), null);
+            string instanceId = await starter.StartNewAsync(nameof(OchestrateSubscriptionCleanUp), null).ConfigureAwait(false);
             _logger.LogInformation($"Started orchestration with ID = '{instanceId}'.");
         }
 
         [FunctionName(nameof(OchestrateSubscriptionCleanUp))]
         public async Task OchestrateSubscriptionCleanUp([OrchestrationTrigger] IDurableOrchestrationContext context)
         {
+            if (context is null)
+            {
+                throw new System.ArgumentNullException(nameof(context));
+            }
+
             if (!context.IsReplaying)
             {
                 _logger.LogDebug("Getting resource groups");
             }
-            var resourceGroupNames = await context.CallActivityAsync<IEnumerable<string>>(nameof(GetResourceGroupsNames), null);
+            var resourceGroupNames = await context.CallActivityAsync<IEnumerable<string>>(nameof(GetResourceGroupsNames), null).ConfigureAwait(false);
 
             foreach (var name in resourceGroupNames)
             {
@@ -43,15 +56,20 @@ namespace Protacon.AzureSubscriptionCleaner.AzureFunctions
                 {
                     _logger.LogDebug("Calling delete function on {resourceGroupName}", name);
                 }
-                await context.CallActivityAsync(nameof(DeleteIfNotLocked), name);
+                await context.CallActivityAsync(nameof(DeleteIfNotLocked), name).ConfigureAwait(false);
             }
         }
 
         [FunctionName(nameof(GetResourceGroupsNames))]
         public async Task<IEnumerable<string>> GetResourceGroupsNames([ActivityTrigger] IDurableActivityContext context)
         {
+            if (context is null)
+            {
+                throw new System.ArgumentNullException(nameof(context));
+            }
+
             _logger.LogTrace("Instance {instanceId}: Finding resource groups to delete...", context.InstanceId);
-            var resourceGroups = await _azureConnection.ResourceGroups.ListAsync(true);
+            var resourceGroups = await _azureConnection.ResourceGroups.ListAsync(true).ConfigureAwait(false);
             _logger.LogDebug("Instance {instanceId}: Found {count} resource groups to delete.", context.InstanceId, resourceGroups.Count());
             return resourceGroups.Select(rg => rg.Name).ToList();
         }
@@ -59,8 +77,13 @@ namespace Protacon.AzureSubscriptionCleaner.AzureFunctions
         [FunctionName(nameof(DeleteIfNotLocked))]
         public async Task DeleteIfNotLocked([ActivityTrigger] string name)
         {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new System.ArgumentException("resource group name is required!", nameof(name));
+            }
+
             _logger.LogDebug("Checking if resource group {resourceGroup} has locks...", name);
-            var locks = await _azureConnection.ManagementLocks.ListByResourceGroupAsync(name, true);
+            var locks = await _azureConnection.ManagementLocks.ListByResourceGroupAsync(name, true).ConfigureAwait(false);
 
             if (locks.Any())
             {
@@ -72,7 +95,7 @@ namespace Protacon.AzureSubscriptionCleaner.AzureFunctions
                 return;
             }
             _logger.LogInformation("Deleting resource group {resourceGroup}", name);
-            await _azureConnection.ResourceGroups.DeleteByNameAsync(name);
+            await _azureConnection.ResourceGroups.DeleteByNameAsync(name).ConfigureAwait(false);
         }
     }
 }
